b25b576aef56faae51356cede92883ba84069d1e
tarasborov <tboro@softserveinc.com>
2020-10-16 15:18:47 +0300

    contact sensor

---
+++ rs_contact_sensor/ContactPlugin.cc
@@ -0,0 +1,62 @@
+#include "ContactPlugin.hh"
+
+using namespace gazebo;
+GZ_REGISTER_SENSOR_PLUGIN(ContactPlugin)
+
+/////////////////////////////////////////////////
+ContactPlugin::ContactPlugin() : SensorPlugin()
+{
+}
+
+/////////////////////////////////////////////////
+ContactPlugin::~ContactPlugin()
+{
+}
+
+/////////////////////////////////////////////////
+void ContactPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr /*_sdf*/)
+{
+  // Get the parent sensor.
+  this->parentSensor =
+    std::dynamic_pointer_cast<sensors::ContactSensor>(_sensor);
+
+  // Make sure the parent sensor is valid.
+  if (!this->parentSensor)
+  {
+    gzerr << "ContactPlugin requires a ContactSensor.\n";
+    return;
+  }
+
+  // Connect to the sensor update event.
+  this->updateConnection = this->parentSensor->ConnectUpdated(
+      std::bind(&ContactPlugin::OnUpdate, this));
+
+  // Make sure the parent sensor is active.
+  this->parentSensor->SetActive(true);
+}
+
+/////////////////////////////////////////////////
+void ContactPlugin::OnUpdate()
+{
+  // Get all the contacts.
+  msgs::Contacts contacts;
+  contacts = this->parentSensor->Contacts();
+  for (unsigned int i = 0; i < contacts.contact_size(); ++i)
+  {
+    std::cout << "Collision between[" << contacts.contact(i).collision1()
+              << "] and [" << contacts.contact(i).collision2() << "]\n";
+
+    for (unsigned int j = 0; j < contacts.contact(i).position_size(); ++j)
+    {
+      std::cout << j << "  Position:"
+                << contacts.contact(i).position(j).x() << " "
+                << contacts.contact(i).position(j).y() << " "
+                << contacts.contact(i).position(j).z() << "\n";
+      std::cout << "   Normal:"
+                << contacts.contact(i).normal(j).x() << " "
+                << contacts.contact(i).normal(j).y() << " "
+                << contacts.contact(i).normal(j).z() << "\n";
+      std::cout << "   Depth:" << contacts.contact(i).depth(j) << "\n";
+    }
+  }
+}
